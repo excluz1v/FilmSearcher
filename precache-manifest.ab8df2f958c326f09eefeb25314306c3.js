79308f76e181dd9dccc5a68a1ae5322a0cbbaff2
excluz1v <bmd4work@gmail.com>
2020-02-16 18:46:19 +0500

    Updates

---
+++ precache-manifest.ab8df2f958c326f09eefeb25314306c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3494f85c6579d349b6da84c7eebfa152",
+    "url": "/FilmSearcher/index.html"
+  },
+  {
+    "revision": "bdc94acba6a4a345d5bc",
+    "url": "/FilmSearcher/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d493061ea64f67f06616",
+    "url": "/FilmSearcher/static/css/main.8b985c9b.chunk.css"
+  },
+  {
+    "revision": "bdc94acba6a4a345d5bc",
+    "url": "/FilmSearcher/static/js/2.bdadfb12.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/FilmSearcher/static/js/2.bdadfb12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d493061ea64f67f06616",
+    "url": "/FilmSearcher/static/js/main.d77d93cc.chunk.js"
+  },
+  {
+    "revision": "00fa4975f4172e359d4d",
+    "url": "/FilmSearcher/static/js/runtime-main.0e9c0d0d.js"
+  },
+  {
+    "revision": "579e532e8ee59f5e7b792305ae39e494",
+    "url": "/FilmSearcher/static/media/Spinner-1s-200px.579e532e.svg"
+  }
+]);
