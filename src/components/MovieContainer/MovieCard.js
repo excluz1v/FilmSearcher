5a5541d304126a55ea31cbd415d750711abbccd3
excluz1v <bmd4work@gmail.com>
2020-02-16 21:53:18 +0500

    3

--- src/components/MovieContainer/MovieCard.js
+++ src/components/MovieContainer/MovieCard.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 export default function MovieCard(props) {
     // console.log(props)
     return (
-        <div className="col-lg-3-sm-6  mb-5 ml-2"  >
-            <div className=' card card-body bg-dark text-center'>
+        <div className="col-lg-3 col-sm-6  mb-3 ml-2"  >
+            <div className='row card card-body bg-dark text-center'>
                 <img src={props.movies.Poster} className=" mb-2" alt="..." />
                 <h5 className="card-title text-light">{props.movies.Title}</h5>
                 <p className="card-text text-light"> {props.movies.Year}</p>
