00860b59e2c0698482caeb985a96f748be1bf23a
excluz1v <bmd4work@gmail.com>
2020-02-26 21:55:59 +0500

    img bug fixed

--- src/components/MovieContainer/MovieCard.js
+++ src/components/MovieContainer/MovieCard.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 export default function MovieCard(props) {
     // console.log(props)
     return (
-        <div className="col-lg-3 col-sm-6  mb-3 ml-2"  >
+        <div className="col-lg-3 col-sm-6 mb-3"  >
             <div className='row card card-body bg-dark text-center'>
-                <img src={props.movies.Poster} className=" mb-2" alt="..." />
+                <img src={props.movies.Poster} style={{ height: "100%", width: "100%" }} alt="..." />
                 <h5 className="card-title text-light">{props.movies.Title}</h5>
                 <p className="card-text text-light"> {props.movies.Year}</p>
                 <Link className='btn btn-primary' to={`/movie/${props.movies.imdbID}`}>
